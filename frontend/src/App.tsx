a5e14f9b0b91296f58d20e3f1fd2e5adccc96091
Rafyy2102 (Rafaela dos Santos Silva) <rafpromais@yahoo.com.br>
2021-09-07 14:27:54 -0300

    componentes fixo básicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
+import DataTable from "components/DataTable";
 import NavBar from "components/NavBar";
+import Footer from "components/NavBar/Footer";
 
 function App() {
   return (
-    <div >
-     <h1 className="text-primary">Olá Estou aqui!!</h1>
-     <NavBar/>
-    </div>
+    <>
+      <NavBar/>
+      <div className="container">
+        <h1 className="text-primary">Olá Estou aqui!!</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 export default App;
---
+++ frontend/src/components/NavBar/Footer/index.tsx
@@ -0,0 +1,13 @@
+import ImgDsDark from 'assets/img/ds-dark.svg'
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Rafyy2102/" target="_blank" rel="noreferrer">Rafyy2102</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
