3e9238e52a60eeeeb9cb4be62042d058594dc1ee
Rafyy2102 (Rafaela dos Santos Silva) <rafpromais@yahoo.com.br>
2021-09-06 23:40:05 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import NavBar from "components/NavBar";
+
 function App() {
   return (
     <div >
      <h1 className="text-primary">Olá Estou aqui!!</h1>
+     <NavBar/>
     </div>
   );
 }
-
 export default App;
